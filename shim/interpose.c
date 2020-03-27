f147d6332f3f0d7e4df585431ed52629a1e463fe
Rajath Shashidhara <rajath.shashidhara@gmail.com>
2020-03-26 23:45:11 -0500

    Add Shim library to interpose NFP BSP tools
    Signed-off-by: Rajath Shashidhara <rajath.shashidhara@gmail.com>

---
+++ shim/interpose.c
@@ -0,0 +1,348 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <errno.h>
+
+#define __USE_GNU
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <asm-generic/ioctl.h>
+#include <unistd.h>
+#include <dlfcn.h>
+#include <pthread.h>
+
+#include "nfp_ioctl.h"
+
+#define MEM_BARRIER() __asm__ volatile("" ::: "memory")
+
+static int (*libc_open)(const char* pathname, int flags) = NULL;
+static int (*libc_openat)(int dirfd, const char* pathname,
+    int flags) = NULL;
+static int (*libc_close)(int fd) = NULL;
+static ssize_t (*libc_pread)(int fd, void* buf, size_t count,
+            off_t offset) = NULL;
+static ssize_t (*libc_pwrite)(int fd, const void* buf,
+        size_t count, off_t offset) = NULL;
+static int (*libc_ioctl)(int fd, unsigned long request, char* argp);
+
+#define MAX_FD  1024 * 1024
+
+enum fd_type {
+    FD_UNUSED,
+    FD_CPP,
+    FD_LIBC
+};
+
+enum op_type {
+    OP_UNUSED,
+    OP_PREAD,
+    OP_PWRITE,
+    OP_IOCTL
+};
+
+static int fd_status[MAX_FD];
+static inline void ensure_init(void);
+
+int open(const char* pathname, int flags)
+{
+    int fd;
+    int temp;
+    struct sockaddr address;
+
+    ensure_init();
+
+    if (strcmp(pathname, "/dev/nfp-cpp-0") == 0)
+    {
+        fd = socket(AF_UNIX, SOCK_STREAM, 0);
+        if (fd < 0)
+            return -1;
+        
+        if (fd >= MAX_FD)
+        {
+            errno = EMFILE;
+            return -1;
+        }
+
+        memset(&address, 0, sizeof(struct sockaddr));
+        address.sa_family = AF_UNIX;
+        strcpy(address.sa_data, "/tmp/nfp_cpp");
+        if (connect(fd, &address, sizeof(struct sockaddr)) < 0)
+        {
+            temp = errno;
+            libc_close(fd);
+            errno = temp;
+
+            return -1;
+        }
+
+        fd_status[fd] = FD_CPP;
+        return 0;
+    }
+    else
+    {
+        return libc_open(pathname, flags);
+    }
+}
+
+int openat(int dirfd, const char* pathname, int flags)
+{
+    ensure_init();
+
+    if (pathname[0] == '/')
+    {
+        return open(pathname, flags);
+    }
+    else
+    {
+        fprintf(stderr, "Relative addressing not supported!\n");
+        
+        errno = ENOTDIR;
+        return -1;
+    }
+}
+
+/* TODO: add error checking */
+int close(int fd)
+{
+    ensure_init();
+
+    int ret = close(fd);
+
+    if (ret == 0)
+    {
+        fd_status[fd] = FD_UNUSED;
+    }
+
+    return ret;
+}
+
+ssize_t pread(int fd, void* buf, size_t count, off_t offset)
+{
+    ssize_t ret;
+
+    ensure_init();
+
+    if (fd_status[fd] == FD_CPP)
+    {
+        uint64_t temp;
+        ssize_t len;
+
+        temp = (uint64_t) OP_PREAD;
+        ret = write(fd, &temp, sizeof(temp));
+        if (ret < sizeof(temp))
+            goto handle_pread_error;
+
+        temp = (uint64_t) count;
+        ret = write(fd, &temp, sizeof(temp));
+        if (ret < sizeof(temp))
+            goto handle_pread_error;
+
+        temp = (uint64_t) offset;
+        ret = write(fd, &temp, sizeof(temp));
+        if (ret < sizeof(temp))
+            goto handle_pread_error;
+
+        ret = read(fd, &temp, sizeof(temp));
+        if (ret < sizeof(temp))
+            goto handle_pread_error;
+        
+        if (temp < count)
+            goto handle_pread_error;
+
+        len = 0;
+        while (len < count)
+        {
+            ret = read(fd, ((char*) buf) + len, count - len);
+            if (ret < 0)
+                goto handle_pread_error;
+            len += ret;
+        }
+
+        return count;
+
+handle_pread_error:
+        fprintf(stderr, "Error when writing to socket: %s",
+            strerror(errno));
+        close(fd);
+        errno = EIO;
+        return -1;
+    }
+    else
+        return libc_pread(fd, buf, count, offset);
+}
+
+ssize_t pwrite(int fd, const void* buf, size_t count, off_t offset)
+{
+    ssize_t ret;
+
+    ensure_init();
+
+    if (fd_status[fd] == FD_CPP)
+    {
+        uint64_t temp;
+        ssize_t len;
+
+        temp = (uint64_t) OP_PWRITE;
+        ret = write(fd, &temp, sizeof(temp));
+        if (ret < sizeof(temp))
+            goto handle_pwrite_error;
+
+        temp = (uint64_t) count;
+        ret = write(fd, &temp, sizeof(temp));
+        if (ret < sizeof(temp))
+            goto handle_pwrite_error;
+
+        temp = (uint64_t) offset;
+        ret = write(fd, &temp, sizeof(temp));
+        if (ret < sizeof(temp))
+            goto handle_pwrite_error;
+
+        len = 0;
+        while (len < count)
+        {
+            ret = write(fd, ((char*) buf) + len, count - len);
+            if (ret < 0)
+                goto handle_pwrite_error;
+            len += ret;
+        }
+
+        ret = read(fd, &temp, sizeof(temp));
+        if (ret < sizeof(temp))
+            goto handle_pwrite_error;
+
+        return temp;
+
+handle_pwrite_error:
+        fprintf(stderr, "Error when writing to socket: %s",
+            strerror(errno));
+        close(fd);
+        errno = EIO;
+        return -1;
+    }
+    else
+        return libc_pwrite(fd, buf, count, offset);
+}
+
+int ioctl(int fd, unsigned long request, char* argp)
+{
+    ensure_init();
+
+    if (fd_status[fd] == FD_CPP)
+    {
+        struct nfp_cpp_area_request area_req;
+        struct nfp_cpp_event_request event_req;
+        struct nfp_cpp_explicit_request explicit_req;
+        struct nfp_cpp_identification ident;
+        uint64_t arg_size, temp;
+        int ret;
+
+        switch (request)
+        {
+        case NFP_IOCTL_CPP_IDENTIFICATION:
+            if (!argp)
+                return sizeof(ident);
+
+            arg_size = sizeof(ident.size);
+            break;
+
+        case NFP_IOCTL_FIRMWARE_LOAD:
+        case NFP_IOCTL_FIRMWARE_LAST:
+            arg_size = NFP_FIRMWARE_MAX;
+            break;
+        
+        case NFP_IOCTL_CPP_AREA_REQUEST:
+        case NFP_IOCTL_CPP_AREA_RELEASE:
+            arg_size = sizeof(area_req);
+            break;
+        
+        case NFP_IOCTL_CPP_AREA_RELEASE_OBSOLETE:
+            arg_size = sizeof(area_req.offset);
+            break;
+        
+        case NFP_IOCTL_CPP_EXPL_REQUEST:
+            arg_size = sizeof(explicit_req);
+            break;
+
+        case NFP_IOCTL_CPP_EVENT_ACQUIRE:
+        case NFP_IOCTL_CPP_EVENT_RELEASE:
+            arg_size = sizeof(event_req);
+            break;
+
+        default:
+            errno = EINVAL;
+            return -1;
+        }
+
+        temp = request;
+        ret = write(fd, &temp, sizeof(temp));
+        if (ret < sizeof(temp))
+            goto handle_ioctl_error;
+        ret = write(fd, argp, arg_size);
+        if (ret < arg_size)
+            goto handle_ioctl_error;
+
+        ret = read(fd, &temp, sizeof(temp));
+        if (ret < sizeof(temp))
+            goto handle_ioctl_error;
+        
+        return (int)temp;
+
+handle_ioctl_error:
+        fprintf(stderr, "Error when writing to socket: %s",
+            strerror(errno));
+        close(fd);
+        errno = EIO;
+        return -1;
+    }
+    else
+        return libc_ioctl(fd, request, argp);
+}
+
+static void *bind_symbol(const char *sym)
+{
+  void *ptr;
+  if ((ptr = dlsym(RTLD_NEXT, sym)) == NULL) {
+    fprintf(stderr, "flextcp socket interpose: dlsym failed (%s)\n", sym);
+    abort();
+  }
+  return ptr;
+}
+
+static void init(void)
+{
+    libc_open = bind_symbol("open");
+    libc_openat = bind_symbol("openat");
+    libc_close = bind_symbol("close");
+    libc_pread = bind_symbol("pread");
+    libc_pwrite = bind_symbol("write");
+    libc_ioctl = bind_symbol("ioctl");
+}
+
+/**
+ * TODO: Handle multi-threaded cases
+ */
+static inline void ensure_init(void)
+{
+    static volatile uint32_t init_cnt = 0;
+    static volatile uint8_t init_done = 0;
+
+    if (init_done == 0)
+    {
+
+        if (__sync_fetch_and_add(&init_cnt, 1) == 0)
+        {
+            init();
+            MEM_BARRIER();
+            init_done = 1;
+        } 
+        else
+        {
+            while (init_done == 0)
+            {
+                pthread_yield();
+            }
+            MEM_BARRIER();
+        }
+    }
+}
